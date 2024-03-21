e0024ba1efca8b56282cf7d017ac9b807a04b766
MaxsimJSDeveloper <147420858+MaxsimJSDeveloper@users.noreply.github.com>
2024-03-21 15:55:04 +0000

    Deploying to gh-pages from @ MaxsimJSDeveloper/js-pet-projects@abb85eac0929c6ce1f05fd6f27e88805ee76ca53 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{g as e}from"./assets/getRandomHexColor-580a5c50.js";const n=document.querySelector(".change-color"),t=document.querySelector(".color");n.addEventListener("click",c);function c(){const o=e();document.body.style.backgroundColor=o,t.innerText=o,console.log(o)}
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{g as t}from"./assets/getRandomHexColor-580a5c50.js";const e=document.querySelector(".change-color"),n=document.querySelector(".color");e.addEventListener("click",c);function c(){const o=t();document.body.style.backgroundColor=o,n.innerText=o}
 //# sourceMappingURL=commonHelpers.js.map
